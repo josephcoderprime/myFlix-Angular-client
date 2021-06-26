0a5ccfc11037faa0421be43979be6bba9a3c4dea
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-26 20:15:49 +0100

    Create user-profile.component.ts

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+// API Call
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// Angular Material
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+// Component
+import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-update.component';
+import { UserProfileDeleteComponent } from '../user-profile-delete/user-profile-delete.component';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  user: any = {};
+  movies: any = [];
+  favorites: any = [];
+
+  /**
+   * @param fetchApiData
+   * @param dialog
+   * @param snackBar
+   * @param router
+   */
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  /**
+  * Updates Profile
+  **/
+  editUserData(): void {
+    this.dialog.open(UserProfileUpdateComponent, {
+      width: '350px'
+    });
+  }
+
+  /**
+  * Delete Profile
+  **/
+  deleteUser(): void {
+    this.dialog.open(UserProfileDeleteComponent);
+  }
+
+  /**
+   * get user data
+   */
+  getUser(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser(user).subscribe((res: any) => {
+      this.user = res;
+      this.getMovies();
+    });
+  }
+
+  /**
+   * retrieve all favorited movies
+   */
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((res: any) => {
+      this.movies = res;
+      this.filterFavorites();
+    });
+  }
+
+  /**
+   * removes movie from user's list of favorites
+   * @param movie_id
+   * @returns
+   */
+  filterFavorites(): void {
+    this.movies.forEach((movie: any) => {
+      if (this.user.FavoriteMovies.includes(movie._id)) {
+        this.favorites.push(movie);
+      }
+    });
+    return this.favorites;
+  }
+
+  /**
+   * delete favorites from user
+   */
+  removeFavorites(id: string, title: string): void {
+    this.fetchApiData.removeFavorite(id).subscribe(() => {
+      this.snackBar.open(`${title} has been removed from your favorites!`, 'OK', {
+        duration: 2000
+      });
+      setTimeout(function () {
+        window.location.reload();
+      }, 2000);
+    });
+  }
+
+}
