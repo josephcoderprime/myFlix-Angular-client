82baf3994fc59fe668c29437f7aba7e11c79624a
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-27 20:52:03 +0100

    Create director-dialog.component.ts

---
+++ src/app/director-dialog/director-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
+
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-dialog',
+  templateUrl: './director-dialog.component.html',
+  styleUrls: ['./director-dialog.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+
+/**
+ * This component provides a dialog/modal for the movie director info.
+ *
+ */
+export class DirectorDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      name: string;
+      bio: string;
+      birth: Date;
+      death: Date;
+    }
+  ) { }
+
+  ngOnInit(): void { }
+}
