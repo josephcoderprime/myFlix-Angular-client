2cedebc003210fdfa2136aa90b3c34dfcbd83333
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-27 20:54:47 +0100

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,132 +1,114 @@
 import { Component, OnInit } from '@angular/core';
-
-// API Calls
 import { FetchApiDataService } from '../fetch-api-data.service';
 
-// Angular material
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-// Components
-import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
-import { MovieDirectorComponent } from '../movie-director/movie-director.component';
-import { MovieGenreComponent } from '../movie-genre/movie-genre.component';
+import { DetailsDialogComponent } from '../details-dialog/details-dialog.component';
+import { DirectorDialogComponent } from '../director-dialog/director-dialog.component';
+import { GenreDialogComponent } from '../genre-dialog/genre-dialog.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.css'],
 })
+
+/**
+ * This component will render various movie data.
+ */
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  favoriteMovieIds: any[] = [];
 
-  /**
-   *
-   * @param fetchApiData
-   * @param dialog
-   * @param snackBar
-   * @param router
-   */
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
-    public snackBar: MatSnackBar,
+    public snackBar: MatSnackBar
   ) { }
 
+  /**
+   * This method will run the getMovies method after the MovieCard Component is initialised and rendered.
+   * @returns array of movie objects.
+   */
   ngOnInit(): void {
     this.getMovies();
-    this.getFavoriteMovies();
   }
 
   /**
-  * retrieve all movies
-  **/
+   * This method will contact an external API and receive an array of movie objects.
+   * @returns array of movie objects.
+   */
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
-      console.log(this.movies);
       return this.movies;
     });
   }
 
   /**
-   *
-   * @param name
-   * @param description
-   * Opens modal with movie genre information
+   * This method will activate dialog/modal which displays various movie data.
+   * @param title movie title
+   * @param imagePath movie image
+   * @param description movie summary
+   * @param director movie director name
+   * @param genre movie genre
    */
-  openGenreDialog(name: string, description: string): void {
-    this.dialog.open(MovieGenreComponent, {
+  showDetailsDialog(
+    title: string,
+    imagePath: string,
+    description: string,
+    director: string,
+    genre: string
+  ): void {
+    this.dialog.open(DetailsDialogComponent, {
+      data: { title, imagePath, description, director, genre },
+      panelClass: 'details-dialog',
+    });
+  }
+
+  /**
+   * This method will activate dialog/modal which displays info on movie genres.
+   * @param name - genre name
+   * @param description - genre summary
+   */
+  showGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreDialogComponent, {
       data: { name, description },
+      panelClass: 'genre-dialog',
     });
   }
 
   /**
-   *
-   * @param name
-   * @param bio
-   * Opens modal with movie director information
+   * This method will activate dialog/modal which displays info on movie director.
+   * @param name - director name
+   * @param bio - director biography
+   * @param birth - director date of birth
+   * @param death - director date of death
    */
-  openDirectorDialog(name: string, bio: string): void {
-    this.dialog.open(MovieDirectorComponent, {
-      data: { name, bio },
+  showDirectorDialog(
+    name: string,
+    bio: string,
+    birth: string,
+    death: string
+  ): void {
+    this.dialog.open(DirectorDialogComponent, {
+      data: { name, bio, birth, death },
+      panelClass: 'director-dialog',
     });
   }
 
   /**
-   *
-   * @param synopsis
-   * Opens modal with movie synopsis information
+   * This method will add movie id to array of favorite movies and show a notificaton it has executed.
+   * @param name - director name
+   * @param bio - director biography
+   * @param birth - director date of birth
+   * @param death - director date of death
    */
-  openSynopsisDialog(synopsis: string): void {
-    this.dialog.open(MovieSynopsisComponent, {
-      data: { synopsis },
-    });
-  }
-
-  /**
-  * check if movie is in favorites
-  **/
-  getFavoriteMovies(): void {
-    const user = localStorage.getItem('user');
-    this.fetchApiData.getUser(user).subscribe((resp: any) => {
-      this.favoriteMovieIds = resp.FavoriteMovies;
-    });
-  }
-
-  /**
-   * checks if movie is in user's list of favorites
-   * @param movie_id
-   * @returns 
-   */
-  isFavorite(movieID: string): boolean {
-    console.log("Movie ID " + movieID + "favorite check");
-    return this.favoriteMovieIds.includes(movieID);
-  };
-
-  /**
-   * Adds or removes movie from user's list of favorites
-   * @param id
-   * @returns
-   */
-  onToggleFavoriteMovie(id: string): any {
-    if (this.isFavorite(id)) {
-      this.fetchApiData.removeFavorite(id).subscribe((resp: any) => {
-        this.snackBar.open('Removed from favorites!', 'OK', {
-          duration: 2000,
-        });
+  addFavorite(id: string, title: string): void {
+    this.fetchApiData.addFavoriteMovie(id).subscribe(() => {
+      this.snackBar.open(`${title} has been added to your favorites!`, 'OK', {
+        duration: 2000,
       });
-      const index = this.favoriteMovieIds.indexOf(id);
-      return this.favoriteMovieIds.splice(index, 1);
-    } else {
-      this.fetchApiData.addFavorite(id).subscribe((response: any) => {
-        this.snackBar.open('Added to favorites!', 'OK', {
-          duration: 2000,
-        });
-      });
-    }
-    return this.favoriteMovieIds.push(id);
+    });
   }
-
 }
